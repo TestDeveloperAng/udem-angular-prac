8ba369b15f8e9f70f9de9289829391a1685f9aeb
TestDeveloperAng <rokdaking1024@gmail.com>
2019-12-29 14:36:53 +0530

    3 files modified

--- src/app/display-server/display-server.component.ts
+++ src/app/display-server/display-server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-display-server',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayServerComponent implements OnInit {
 
+  @Input() server;
   constructor() { }
 
   ngOnInit() {
